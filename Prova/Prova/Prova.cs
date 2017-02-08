ca1ce43b244d8fe13c2c50532ec81e12e57ef4cd
diegoreboucas22 <diegoreboucas22@hotmail.com>
2017-02-08 17:18:42 -0200

    EsTudando git

---
+++ Prova/Prova/Prova.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova
+{
+    public class Prova
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                if ((i % 2) == 0)
+                {
+                    Console.WriteLine("Número {0} é Divisivel por 2", i.ToString());
+                }
+                if ((i % 3) == 0)
+                {
+
+                    Console.WriteLine("Número {0} é Divisivel por 3", i.ToString());
+                }
+
+                if ((i % 2) != 0 && (i % 3) != 0)
+                {
+
+                    Console.WriteLine(i);
+                }
+                if ((i % 2) == 0 && (i % 3) == 0)
+                {
+                    Console.WriteLine("Número {0} é Divisivel por 2 e Divisivel por 3", i.ToString());
+                }
+
+            }
+
+
+            Console.Read();
+        }
+    }
+}
